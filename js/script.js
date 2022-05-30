81adfa2d533be69fb183e6835b5a50fdd37696ac
vladliaskivskyi <38475511+vlyaskovskij@users.noreply.github.com>
2022-05-30 12:42:29 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.animleft').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.animright').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('.animup').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('.animdown').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('.animbounce').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("bounce");
+	        }
+	    });
+	});
+})
